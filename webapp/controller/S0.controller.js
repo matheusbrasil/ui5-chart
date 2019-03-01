a63fc4f93051338177e3a7346a6ce90cef6b3e0e
Matheus Brasil <matheus.brasil@gmail.com>
2019-03-01 00:58:00 +0000

    Criação do grafico

--- webapp/controller/S0.controller.js
+++ webapp/controller/S0.controller.js
@@ -1,24 +1,68 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel"
-], function(Controller, JSONModel) {
+], function (Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ovly.chart.controller.S0", {
 
-		onInit: function() {
+		onInit: function () {
 			this._createChartModel();
-			
-			this.getView().setModel(this.oModel);
+
+			//this.getView().setModel(this.oModel);
+			this.getView().setModel(this.oModel, "chart");
 		},
 
-		_createChartModel: function() {
-			var oModel = new JSONModel({
-				
+		onChange: function (oEvent) {
+			var oItem = oEvent.getParameters().selectedItem;
+			var oContext = oItem.getBindingContext();
+
+			var sTitle = this.getOwnerComponent()
+				.getModel("i18n")
+				.getResourceBundle()
+				.getText("chart_title", oContext.getProperty("Name"));
+
+			this.getView().getModel("chart").setProperty("/properties/title/text", sTitle);
+
+			this.byId("chart").setBindingContext(oContext);
+
+		},
+
+		onSelectData: function (oEvent) {
+			debugger;
+			//cosole.table()
+		},
+
+		_createChartModel: function () {
+
+			var sTitle = this.getOwnerComponent()
+				.getModel("i18n")
+				.getResourceBundle()
+				.getText("chart_title");
+
+			/*			var oModel = new JSONModel({
+
+						});
+
+						this.getView().setModel(oModel, "chart");*/
+
+			this.oModel = new JSONModel({
+				properties: {
+					legendGroup: {
+						layout: {
+							position: "bottom",
+							alignment: "center"
+						}
+					},
+					title: {
+						text: sTitle,
+						visible: true,
+						style: {
+							color: 'orange'
+						}
+					}
+				}
 			});
-
-			this.getView().setModel(oModel, "chart");
-
 		}
 
 	});
